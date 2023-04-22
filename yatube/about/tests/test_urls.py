77ae659d9ce44440af0bb376d354b2e8e5d161e1
Абакар Манатов <akap302@gmail.com>
2023-04-22 22:50:21 +0300

    Прогнал по тестам

--- yatube/about/tests/test_urls.py
+++ yatube/about/tests/test_urls.py
@@ -11,7 +11,7 @@ class StaticPagesURLTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_author_url_uses_correct_template(self):
-        """Проверка шаблона для адреса /about/author/."""     
+        """Проверка шаблона для адреса /about/author/."""
         response = self.guest_client.get('author/')
         self.assertTemplateUsed(response, 'author.html')
 
@@ -21,6 +21,6 @@ class StaticPagesURLTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_tech_url_uses_correct_template(self):
-        """Проверка шаблона для адреса /about/tech/."""     
+        """Проверка шаблона для адреса /about/tech/."""
         response = self.guest_client.get('tech/')
         self.assertTemplateUsed(response, 'tech.html')
