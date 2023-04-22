20a86c9b1c75539f1132472b0bafcecdd9f8b722
Абакар Манатов <akap302@gmail.com>
2023-04-22 22:22:34 +0300

    Прогнал по тестам

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -36,44 +36,33 @@ class PostURLTests(TestCase):
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
 
-    def test_home_url_exists_at_desired_location(self):
-        """Страница / доступна любому пользователю."""
-        response = self.guest_client.get('/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_group_url_exists_at_desired_location(self):
-        """Страница /group/<slug>/ доступна любому пользователю."""
-        response = self.guest_client.get(f'/group/{self.group.slug}/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_profile_url_exists_at_desired_location(self):
-        """Страница /profile/<str:username>/ доступна любому пользователю."""
-        response = self.guest_client.get(f'/profile/{self.post.author}/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_post_detail_url_exists_at_desired_location(self):
-        """Страница /posts/<int:post_id>/ доступна любому пользователю."""
-        response = self.guest_client.get(f'/posts/{self.post.pk}/')
-        self.assertEqual(response.status_code, 200)
+    def test_urls_uses_correct_template_all_users(self):
+            templates_url_names = {
+                'posts/index.html': '/',
+                'posts/group_list.html': f'/group/{self.group.slug}/',
+                'posts/profile.html': f'/profile/{self.post.author}/',
+                'posts/post_detail.html': f'/posts/{self.post.pk}/',
+            }
+            for template, address in templates_url_names.items():
+                with self.subTest(address=address):
+                    response = self.guest_client.get(address)
+                    self.assertTemplateUsed(response, template)
 
     def test_create_url_exists_at_desired_location(self):
-        """Страница /create/ доступна авторизованному пользователю."""
-        response = self.authorized_client.get('/create/')
-        self.assertEqual(response.status_code, 200)
+            templates_url_names = {
+                'posts/create_post.html': '/create/',
+                'posts/post_edit.html': f'/posts/{self.post.pk}/edit/',
+            }
+            for template, address in templates_url_names.items():
+                with self.subTest(address=address):
+                    response = self.authorized_client.get(address)
+                    self.assertTemplateUsed(response, template)
+
+    def test_post_edit_url_redirect_anonymous(self):
+        response = self.guest_client.get(f'/posts/{self.post.pk}/edit/')
+        self.assertEqual(response.status_code, 302)
 
     def test_create_url_redirect_anonymous(self):
         """Страница /create/ перенаправляет анонимного пользователя."""
         response = self.guest_client.get('/create/')
-        self.assertEqual(response.status_code, 302)
-
-    def test_post_edit_url_exists_at_desired_location(self):
-        """Страница /posts/<int:post_id>/edit/ доступна авторизованному пользователю."""
-        response = self.authorized_client.get(
-                        f'/posts/{self.post.pk}/edit/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_post_edit_url_redirect_anonymous(self):
-        """Страница /posts/<int:post_id>/edit/ перенаправляет анонимного пользователя."""
-        response = self.guest_client.get(
-            f'/posts/{self.post.pk}/edit/')
         self.assertEqual(response.status_code, 302)
